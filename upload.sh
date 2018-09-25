aa6ee45fa87bc8ae5affd59cb1ac782352084a93
diaoxinrui <443135581@qq.com>
2018-09-25 14:27:23 +0800

    date

--- upload.sh
+++ upload.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 tarName="KH.tar.gz"
+commitMark=date
 if [[ -f $tarName ]];then
   rm -rf $tarName 
 fi
@@ -7,5 +8,5 @@ git pull
 gulp pack
 tar -czf  $tarName ./KH
 git add .
-git commit -m `"提交"${date}`
-git push
+git commit -m ${commitMark}
+# git push
